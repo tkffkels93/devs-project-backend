aa4ae10486119801d19e94f4fe21d19e97f2a195
tkffkels93 <tkffkels93@gmail.com>
2024-07-10 17:44:15 +0900

    추가 세팅

--- src/main/java/com/example/devs/model/reply/ReplyRestController.java
+++ src/main/java/com/example/devs/model/reply/ReplyRestController.java
@@ -1,9 +1,11 @@
 package com.example.devs.model.reply;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RequestMapping("/api/reply")
 @RestController
 public class ReplyRestController {
 }
--- src/main/java/com/example/devs/model/user/UserRestController.java
+++ src/main/java/com/example/devs/model/user/UserRestController.java
@@ -1,10 +1,12 @@
 package com.example.devs.model.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 @RestController
-public class UserController {
+public class UserRestController {
     private final UserService userService;
 }
