fdedb0f3be797efefdcd99b3ba0fef40d77cd463
sw-kim <kimsaewoon@gmail.com>
2024-07-16 14:36:23 +0900

    내글에 북마크한경우표시 완료

--- src/main/java/com/example/devs/model/bookmark/BookmarkService.java
+++ src/main/java/com/example/devs/model/bookmark/BookmarkService.java
@@ -1,9 +1,15 @@
 package com.example.devs.model.bookmark;
 
+import com.example.devs._core.enums.BoardRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class BookmarkService {
+    private final BookmarkRepository bookmarkRepository;
+    //해당 게시글에 특정 사용자의 like 개수를 가져온다 (여부가져오기)
+    public Integer getBookmarkCount(BoardRole boardRole, Integer boardId, Integer userId) {
+        return bookmarkRepository.countBookmark(boardRole, boardId, userId);
+    }
 }
