207721ec1df3874917abbaf140d248ea0a3c442a
sw-kim <kimsaewoon@gmail.com>
2024-07-17 12:45:36 +0900

    게시글 내용&댓글내용 가져오기 완료

--- src/main/java/com/example/devs/model/reply/ReplyService.java
+++ src/main/java/com/example/devs/model/reply/ReplyService.java
@@ -1,9 +1,18 @@
 package com.example.devs.model.reply;
 
+import com.example.devs._core.enums.BoardRole;
+import com.example.devs.model.board.BoardResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ReplyService {
+    private final ReplyRepository replyRepository;
+    public List<BoardResponse.ReplyDTO> getReplies(BoardRole boardRole, Integer boardId) {
+        List<Reply> replies = replyRepository.findByBoardId(boardRole, boardId);
+        return replies.stream().map(BoardResponse.ReplyDTO::new).toList();
+    }
 }
