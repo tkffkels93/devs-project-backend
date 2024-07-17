55ebb16cdd8f7adfb3d0f7cef32918cf36a4abd5
sik2Boii <junsik213@naver.com>
2024-07-17 16:05:48 +0900

    Feat: 마이페이지 데이터 조회

--- src/main/java/com/example/devs/model/user/UserRepository.java
+++ src/main/java/com/example/devs/model/user/UserRepository.java
@@ -3,6 +3,10 @@ package com.example.devs.model.user;
 import com.example.devs._core.enums.UserProvider;
 import com.example.devs._core.enums.UserRole;
 import com.example.devs._core.enums.UserStatus;
+import com.example.devs.model.board.Board;
+import com.example.devs.model.reply.Reply;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -21,9 +25,17 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findByRoleAndStatusIn(@Param("role") UserRole role, @Param("status") List<UserStatus> status);
 
     @Query("select u from User u where u.email = :email and u.provider = 'EMAIL'")
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(@Param("email") String email);
 
     // 이메일로 회원정보 조회
     @Query("select u from User u where u.email = :email and u.provider = :provider")
     Optional<User> findByEmailAndProvider(@Param("email") String email, @Param("provider") UserProvider provider);
+
+    // 내가 작성한 게시판 조회
+    @Query("SELECT b FROM Board b WHERE b.user.id = :id")
+    Page<Board> findMyBoardsById(@Param("id") Integer id, Pageable pageable);
+
+    // 내가 작성한 댓글 조회
+    @Query("SELECT r FROM Reply r JOIN r.board b WHERE r.user.id = :id")
+    Page<Reply> findMyRepliesById(@Param("id") Integer id, Pageable pageable);
 }
