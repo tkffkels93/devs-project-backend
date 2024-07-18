984b1186ff2d16a7f56a0af696f7fc5e8e40d22e
tkffkels93 <tkffkels93@gmail.com>
2024-07-18 17:33:57 +0900

    관리자 / 북마크 추가, 삭제

--- src/main/java/com/example/devs/model/bookmark/BookmarkRepository.java
+++ src/main/java/com/example/devs/model/bookmark/BookmarkRepository.java
@@ -8,11 +8,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface BookmarkRepository extends JpaRepository<Bookmark, Integer> {
 
     //특정 게시글 & 특정 사용자의 북마크 개수를 가져온다.
     @Query("select count(b) from Bookmark b where b.boardRole = :boardRole and b.board.id = :boardId and b.user.id = :userId")
     Integer countBookmark(@Param("boardRole") BoardRole boardRole, @Param("boardId") Integer boardId, @Param("userId") Integer userId);
+
+    // 해당 회원이 북마크한 게시글 찾기
+    @Query("select b from Bookmark b where b.user.id = :userId and b.board.id = :boardId")
+    Optional<Bookmark> findByUserIdAndBoardId(@Param("userId") Integer userId, @Param("boardId") Integer boardId);
 
     // 회원이 저장한 북마크 찾기
     @Query("SELECT bm FROM Bookmark bm JOIN FETCH bm.board b JOIN FETCH b.user u WHERE bm.user.id = :userId and b.status = :status ORDER BY b.createdAt DESC")
